abf89127a27f8577012216b874dae183c2c167f9
Andrea Biondo <andrea.biondo965@gmail.com>
2022-02-25 08:55:46 +0100

    Domain interfaces

---
+++ src/domains/domains.ml
@@ -0,0 +1,34 @@
+module type Lattice = sig
+    type t
+    val top : t
+    val bot : t
+    val lte : t -> t -> bool
+    val lub : t -> t -> t
+    val glb : t -> t -> t
+    val to_string : t -> string
+end
+
+module Fixpoint (L : Lattice) = struct
+    let lfp (f : L.t -> L.t) : L.t =
+        let rec iter y =
+            let y' = f y in
+            if L.lte y' y then y else iter y'
+        in iter L.bot
+end
+
+module type StmSemantics = sig
+    type state
+    val abstract_stm : Language.stm -> state -> state
+end
+
+module type ExprSemantics = sig
+    type state
+    type value
+    val abstract_a_expr: Language.a_expr -> state -> value
+    val abstract_b_expr: Language.b_expr -> state -> state
+end
+
+module type Domain = sig
+    module StateLat : Lattice
+    module StmSem : StmSemantics with type state = StateLat.t
+end
