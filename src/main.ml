21438e0d00d9eccae2e084b975dc14c354fb000d
Andrea Biondo <andrea.biondo965@gmail.com>
2022-02-25 08:56:57 +0100

    Main driver

--- src/main.ml
+++ src/main.ml
@@ -1,10 +1,4 @@
-module L = Language
-module S = Semantics
-module VF = VariableFinder
-open Utils
-
-(* Parses a program *)
-let parse (c : in_channel) : L.stm =
+let parse (c : in_channel) : Language.stm =
 	let lexbuf = Lexing.from_channel c in
 	try
 		Parser.program Lexer.token lexbuf
@@ -20,51 +14,11 @@ let parse (c : in_channel) : L.stm =
 			exit 1
 		end
 
-(* Regular expression for var=value arguments *)
-let kv_arg_regex = Re.Posix.compile_pat "([a-zA-Z][a-zA-Z0-9]*)=(0|-?[1-9][0-9]*)"
-
-(* Builds a state with initialized variables for the given arguments *)
-let rec argument_state (a : string list) : S.state =
-	let rec build n l =
-		match l with
-		| [] -> S.State.empty
-		| x :: xs -> match Re.exec_opt kv_arg_regex x with
-			| None      -> S.State.add ("$" ^ (string_of_int n)) (Z.of_string x) (build (n+1) xs)
-			| Some (ms) -> S.State.add (Re.get ms 1) (Z.of_string @@ Re.get ms 2) (build n xs)
-	in build 0 a
-
-(* Fills in missing state variables with random values *)
-let fill_state (vars : VF.VarSet.t) (s : S.state) : S.state =
-	let f x s0 = if S.State.mem x s0 then s0 else S.State.add x (random_z ()) s0 in
-	VF.VarSet.fold f vars s
-
-(* Builds the initial state, given a program and a list of arguments *)
-let initial_state (program : L.stm) (a : string list) : S.state =
-	fill_state (VF.variables program) (argument_state a)
-
-(* Prints a state to the standard output *)
-let dump_state s =
-	S.State.iter (fun k v -> Printf.printf "%6s -> %s\n" k (Z.to_string v)) s
+module Domain : Domains.Domain = IntervalDomain
 
 let program = parse stdin
-let args = Array.sub Sys.argv 1 (Array.length Sys.argv - 1)
-let si = initial_state program @@ Array.to_list args
+
+let final_state = Domain.StmSem.abstract_stm program Domain.StateLat.top
 
 let () =
-	Printf.printf("---------------------\n");
-	Printf.printf("    INITIAL STATE    \n");
-	Printf.printf("---------------------\n");
-	dump_state si;
-	Printf.printf("---------------------\n\n");
-
-	Printf.printf("Running...\n\n");
-	flush stdout;
-
-	let sf = S.semantic program si in
-
-	Printf.printf("---------------------\n");
-	Printf.printf("     FINAL STATE     \n");
-	Printf.printf("---------------------\n");
-	dump_state sf;
-	Printf.printf("---------------------\n");
-	flush stdout;
+	Printf.printf "%s\n" (Domain.StateLat.to_string final_state)
