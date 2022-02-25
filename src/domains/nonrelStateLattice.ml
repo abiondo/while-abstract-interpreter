b166afa09314009ab65cae6ebaddd42aa0d58888
Andrea Biondo <andrea.biondo965@gmail.com>
2022-02-25 10:20:22 +0100

    NonrelStateLattice: tidy up

--- src/domains/nonrelStateLattice.ml
+++ src/domains/nonrelStateLattice.ml
@@ -16,6 +16,13 @@ module Make (ValueLat : Domains.Lattice) = struct
         let ks = List.map fst bs in
         VarSet.of_list ks
 
+    let binop (f : ValueLat.t -> ValueLat.t -> ValueLat.t)
+              (m1 : ValueLat.t State.StateMap.t)
+              (m2 : ValueLat.t State.StateMap.t) =
+        let vf x = f (State.get x (Elm(m1))) (State.get x (Elm(m2))) in
+        let update x s = State.set x (vf x) s in
+        VarSet.fold update (vars [m1; m2]) top
+
     let lte (s1 : t) (s2 : t) : bool =
         match s1, s2 with
         | Bot, _ -> true
@@ -27,26 +34,17 @@ module Make (ValueLat : Domains.Lattice) = struct
     let lub (s1 : t) (s2 : t) : t =
         match s1, s2 with
         | Bot, s | s, Bot -> s
-        | Elm(m1), Elm(m2) ->
-            let vlub x = ValueLat.lub (State.get x s1) (State.get x s2) in
-            let update x s = State.set x (vlub x) s in
-            VarSet.fold update (vars [m1; m2]) top
+        | Elm(m1), Elm(m2) -> binop ValueLat.lub m1 m2
 
     let glb (s1 : t) (s2 : t) : t =
         match s1, s2 with
         | Bot, _ | _, Bot -> bot
-        | Elm(m1), Elm(m2) ->
-            let vglb x = ValueLat.glb (State.get x s1) (State.get x s2) in
-            let update x s = State.set x (vglb x) s in
-            VarSet.fold update (vars [m1; m2]) top
+        | Elm(m1), Elm(m2) -> binop ValueLat.glb m1 m2
 
     let widen (s1 : t) (s2 : t) : t =
         match s1, s2 with
         | Bot, s | s, Bot -> s
-        | Elm(m1), Elm(m2) ->
-            let vwiden x = ValueLat.widen (State.get x s1) (State.get x s2) in
-            let update x s = State.set x (vwiden x) s in
-            VarSet.fold update (vars [m1; m2]) top
+        | Elm(m1), Elm(m2) -> binop ValueLat.widen m1 m2
 
     let to_string (s : t) : string =
         match s with
