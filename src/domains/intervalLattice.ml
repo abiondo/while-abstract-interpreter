ce66abd767f641c9cf752b9bbe493ba8444dd7b0
Andrea Biondo <andrea.biondo965@gmail.com>
2022-02-25 10:09:41 +0100

    IntervalLattice: fix swapped lub/glb bots

--- src/domains/intervalLattice.ml
+++ src/domains/intervalLattice.ml
@@ -11,12 +11,12 @@ let lte (v1 : t) (v2 : t) : bool =
 
 let lub (v1 : t) (v2 : t) : t =
     match v1, v2 with
-    | Bot, _ | _, Bot -> bot
+    | Bot, v | v, Bot -> v
     | Interval(a1, b1), Interval(a2, b2) -> ZInf.(Interval(min a1 a2, max b1 b2))
 
 let glb (v1 : t) (v2 : t) : t =
     match v1, v2 with
-    | Bot, v | v, Bot -> v
+    | Bot, _ | _, Bot -> bot
     | Interval(a1, b1), Interval(a2, b2) ->
         let a' = ZInf.max a1 a2 in
         let b' = ZInf.min b1 b2 in
