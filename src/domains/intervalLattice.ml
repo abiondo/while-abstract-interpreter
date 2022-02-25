1da26725dd050d73f559f8ea4ed4d51bdb612520
Andrea Biondo <andrea.biondo965@gmail.com>
2022-02-25 09:07:25 +0100

    IntervalLattice: remove ZI = ZInf

--- src/domains/intervalLattice.ml
+++ src/domains/intervalLattice.ml
@@ -1,6 +1,4 @@
-module ZI = ZInf
-
-type t = Bot | Interval of ZI.t * ZI.t
+type t = Bot | Interval of ZInf.t * ZInf.t
 
 let top = Interval(NegInf, PosInf)
 let bot = Bot
@@ -9,22 +7,23 @@ let lte (v1 : t) (v2 : t) : bool =
     match v1, v2 with
     | Bot, _ -> true
     | _, Bot -> false
-    | Interval(a1, b1), Interval(a2, b2) -> ZI.(a1 >= a2 && b1 <= b2)
+    | Interval(a1, b1), Interval(a2, b2) -> ZInf.(a1 >= a2 && b1 <= b2)
 
 let lub (v1 : t) (v2 : t) : t =
     match v1, v2 with
     | Bot, _ | _, Bot -> bot
-    | Interval(a1, b1), Interval(a2, b2) -> ZI.(Interval(min a1 a2, max b1 b2))
+    | Interval(a1, b1), Interval(a2, b2) -> ZInf.(Interval(min a1 a2, max b1 b2))
 
 let glb (v1 : t) (v2 : t) : t =
     match v1, v2 with
     | Bot, v | v, Bot -> v
     | Interval(a1, b1), Interval(a2, b2) ->
-        let a' = ZI.max a1 a2 in
-        let b' = ZI.min b1 b2 in
-        if ZI.(a' <= b') then Interval(a', b') else bot
+        let a' = ZInf.max a1 a2 in
+        let b' = ZInf.min b1 b2 in
+        if ZInf.(a' <= b') then Interval(a', b') else bot
 
 let to_string (v : t) : string =
     match v with
     | Bot -> "bot"
-    | Interval (a, b) -> "[" ^ (ZI.to_string a) ^ ", " ^ (ZI.to_string b) ^ "]"
+    | Interval (a, b) ->
+        "[" ^ (ZInf.to_string a) ^ ", " ^ (ZInf.to_string b) ^ "]"
